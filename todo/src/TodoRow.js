f56acda8e619d7d81fa1b320a407316dc4cd73a9
Your Name <you@example.com>
2025-08-12 09:18:25 -0400

    Added Checkbox Toggle

--- todo/src/TodoRow.js
+++ todo/src/TodoRow.js
@@ -1,28 +1,26 @@
 import React from 'react';
- 
-function TodoRow({item, toggle})
 
+function TodoRow({item, toggle})
 {
+
+    const onToggle = () => {
+        toggle(item);
+    };
+
     return (
         <tr>
-                <td>
-
+            <td>
                 { item.action }
-</td>
-<td>
-<input
-
+            </td>
+            <td>
+                <input
                     type="checkbox"
-
                     checked={ item.done }
-
+                    onChange={ onToggle }
                     />
-</td>
-</tr>
-
+            </td>
+        </tr>
     );
-
 }
- 
+
 export default TodoRow;
- 
